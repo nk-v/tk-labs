5c73204b4e3cf54adf403b0a64ed028cc1ce4b36
nk-v <vidmnick@gmail.com>
2021-09-12 16:15:17 +0400

    Rm compilers

--- src/utils/prints.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.showMatrix = void 0;
-const showMatrix = (matrix, label) => {
-    console.log();
-    label && console.log(label + ":");
-    console.table(matrix.toArray());
-};
-exports.showMatrix = showMatrix;
