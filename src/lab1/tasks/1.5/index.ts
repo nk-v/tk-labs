ea7890660f07bab98f55f2a9d2b1671c551f95c4
kachmazoff <alek.kachmazov@yandex.ru>
2021-09-23 13:55:19 +0400

    last task in lab1

--- src/lab1/tasks/1.5/index.ts
+++ src/lab1/tasks/1.5/index.ts
@@ -1,3 +1,8 @@
+import * as math from "mathjs";
+import { allWords } from "../1.4-first/calcs";
+
+export const getWeight = (row: number[]) => row.reduce((acc, curr) => acc + curr, 0)
+
 /**
  * Нахождение кодового расстояния
  * @param matrix Матрица кодовых слов
@@ -5,9 +10,30 @@
  */
 export const codeDistance = (matrix: math.Matrix) : number => {
     const mtx = matrix.toArray() as number[][];
-    const weights = mtx.map((row) => row.reduce((acc, curr) => acc + curr, 0));
+    const weights = mtx.map(getWeight);
     
     return Math.min(...weights.filter(x => x > 0));
 }
 
 export const getT = (dist: number) => dist - 1;
+
+export const getRandomError = (t: number, n: number) => {
+    if (t >= n) {
+        return Array.from({length: n}, (_ => 1));
+    }
+
+    const v = Array.from({length: n}, (_ => 0));
+    const emptyIndexes = Array.from({length: n}, ((_, ind) => ind));
+
+    for (let i = 0; i < t; i++) {
+        const rnd = Math.floor((Math.random() * emptyIndexes.length)) % emptyIndexes.length;
+        v[emptyIndexes[rnd]] = 1;
+
+        emptyIndexes.splice(rnd, 1);
+    }
+
+    return v;
+}
+
+export const getAllErrors  = (t: number, n: number) =>
+    allWords(n).filter(row => getWeight(row) === t);
--- src/lab1/tasks/1.5/test.ts
+++ src/lab1/tasks/1.5/test.ts
@@ -1,8 +1,40 @@
-import { codesFromXorAllWords } from "../1.4-first/calcs";
-import {codeDistance, getT} from "./index";
-import { G } from "./mocks";
+import * as math from "mathjs";
+import { bitMultiply, codesFromXorAllWords } from "../1.4-first/calcs";
+import {codeDistance, getAllErrors, getRandomError, getT} from "./index";
+import { G, H } from "./mocks";
 
-const d = codeDistance(codesFromXorAllWords(G));
+const codes = codesFromXorAllWords(G);
+const d = codeDistance(codes);
 const t = getT(d);
+const n = 10;
 
 console.log({d, t});
+
+const err = getRandomError(t, n);
+const sum = math.bitXor(err, codes.toArray()[1]);
+const mult = bitMultiply(sum, H);
+
+// console.table({err, v: codes.toArray()[1], vWithErr: sum, result: mult.toArray()});
+
+const checkRes = (v: number[], e: number[], H) => {
+    const sum = math.bitXor(v, e);
+    return bitMultiply(sum, H);
+}
+
+const v = codes.toArray()[1] as number[];
+const errors = getAllErrors(t + 1, n);
+const checks = errors.map(e => checkRes(v, e, H).toArray());
+
+const errIndexies = [];
+checks.forEach((row: number[], currInd: number) => {
+    if (row.every(x => x === 0)) {
+        errIndexies.push(currInd);
+    }
+})
+
+console.log("Вектора ошибок")
+console.table(errors.filter((_, ind) => errIndexies.includes(ind)));
+console.log("Вектора результатов")
+console.table(checks.filter((_, ind) => errIndexies.includes(ind)));
+
+// console.table(errors.map(e => checkRes(v, e, H).toArray()));
