8c5a6549715b134a5c06031f8d6bddc070337eff
nk-v <vidmnick@gmail.com>
2021-10-16 18:30:56 +0400

    lab2 correct

--- src/lab2/index.ts
+++ src/lab2/index.ts
@@ -1,6 +1,7 @@
-import { getAllErrors } from "../lab1/tasks/1.5";
+import { getAllErrors, getRandomError } from "../lab1/tasks/1.5";
 import { bitMultiply } from "../lab1/tasks/1.4-first/calcs";
-import { bitXor } from "mathjs";
+import { bitXor, index } from "mathjs";
+import math = require("mathjs");
 
 const getI = (length: number) =>
   Array.from({ length }, (_, ind) =>
@@ -48,53 +49,143 @@ const solve = (n: number, k: number, d: number, maxErr: number) => {
   const G = I.map((row, ind) => [...row, ...X[ind]]);
   const H = [...X, ...getI(n - k)];
 
-  console.table(I);
+  console.log("X =");
   console.table(X);
+  console.log("G =");
   console.table(G);
+  console.log("H =");
   console.table(H);
 
-  for (let err = 1; err <= maxErr; err++) {
-    const errors = getAllErrors(err, n);
-    console.log("errors");
-    console.table(errors);
+  // for (let err = 1; err <= maxErr; err++) {
+  console
+    .log
+    // "=============== Error multiplicity " + maxErr + " ==============="
+    ();
+  // const errors_1 = getAllErrors(1, n);
 
-    // Генерируем слово длины k
-    const word = getRandomRow(k);
-    // Сформировать кодовое слово длины n из слова длины k.
-    const encodedWord = bitMultiply(word, G);
+  // console.log("ErrsSyndromes 1 :");
+  // console.table(bitMultiply(errors_1, H));
 
-    const randomError = errors[Math.floor(Math.random() * errors.length)];
-    // Подмешиваем ошибку в исходное слово
-    let wrongWord = bitXor(encodedWord, randomError);
+  console.log("ErrsSyndromes 2 :");
+  const errors_2 = getAllErrors(2, n);
+  console.table(bitMultiply(errors_2, H));
+  const randomErr = errors_2[Math.floor(Math.random() * errors_2.length)];
+  console.table({ randomErr, randomSyndrome: bitMultiply(randomErr, H) });
 
-    let step = 0;
-    const TOTAL_STEPS = err;
-    while (++step <= TOTAL_STEPS) {
-      console.log("Step", step);
-      const syndrome = bitMultiply(wrongWord, H);
-      console.table({ word, encodedWord, randomError, wrongWord, syndrome });
+  // const word = getRandomRow(k);
+  // const encodedWord = bitMultiply(word, G);
 
-      const foundStringIndex = findRowIndex(H, syndrome);
-      console.log({ foundStringIndex });
+  // const randomError = errors[Math.floor(Math.random() * errors.length)];
+  // let wrongWord = bitXor(encodedWord, randomError);
 
-      if (foundStringIndex < 0) {
-        console.log("Не нашёл ошибку");
-        console.table({ encodedWord, wrongWord });
+  // console.table("Data = ");
+  // console.table({ word });
+  // console.table({ encodedWord, randomError, wrongWord });
 
-        return;
-      }
+  // let step = 0;
+  // const TOTAL_STEPS = err;
+  // while (++step <= TOTAL_STEPS) {
+  //   console.log("Step :: " + step);
 
-      const vec = Array.from({ length: n }, () => 0);
-      vec[foundStringIndex] = 1;
+  //   const syndrome = bitMultiply(wrongWord, H);
+  //   const foundStringIndex = findRowIndex(H, syndrome);
+  //   console.table({ syndrome, foundStringIndex });
 
-      const checkWord = bitXor(wrongWord, vec);
+  //   if (foundStringIndex < 0) {
+  //     console.log("No syndrome in H");
+  //     console.table({ encodedWord, finalWord: wrongWord });
+  //     return;
+  //   }
 
-      console.table({ encodedWord, checkWord });
-      wrongWord = checkWord;
-      console.log();
-    }
-  }
+  //   const vec = Array.from({ length: n }, () => 0);
+  //   vec[foundStringIndex] = 1;
+
+  //   const checkWord = bitXor(wrongWord, vec);
+  //   const areEqual =
+  //     JSON.stringify(checkWord) === JSON.stringify(encodedWord);
+
+  //   console.table({ wrongWord, vec });
+  //   console.table({ checkWord, encodedWord, areEqual });
+
+  //   wrongWord = checkWord;
+  // }
+  // }
 };
 
-// part1(7, 4, 3, 2);
-solve(20, 10, 5, 2);
+/**
+ * G =
+┌─────────┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+│ (index) │ 0 │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │
+├─────────┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+│    0    │ 1 │ 0 │ 0 │ 1 │ 1 │ 1 │ 0 │ 1 │ 0 │ 0 │
+│    1    │ 0 │ 1 │ 0 │ 1 │ 1 │ 1 │ 1 │ 0 │ 0 │ 0 │
+│    2    │ 0 │ 0 │ 1 │ 1 │ 1 │ 0 │ 0 │ 1 │ 1 │ 1 │
+└─────────┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+ */
+
+// const G = [
+//   [1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
+//   [0, 1, 0, 0, 1, 0, 0, 1, 1, 1],
+//   [0, 0, 1, 0, 0, 1, 1, 1, 1, 1],
+//   [0, 0, 0, 1, 1, 0, 1, 1, 0, 1],
+// ];
+
+const func = () => {
+  const n = 10,
+    k = 4;
+
+  const X = [
+    [0, 1, 1, 0, 1, 1],
+    [1, 0, 0, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 1],
+  ];
+
+  const I = getI(k);
+  const G = I.map((row, ind) => [...row, ...X[ind]]);
+  const H = [...X, ...getI(n - k)];
+
+  console.log("G = ");
+  console.table(G);
+
+  console.log("H = ");
+  console.table(H);
+
+  const multiplicity = 3;
+
+  const randomErr = getRandomError(multiplicity, n);
+  const syndrome = bitMultiply(randomErr, H);
+  const indexes = findHIndexes(syndrome, H, multiplicity);
+
+  const correctVec = Array.from({ length: n }, (_, k) =>
+    indexes.includes(k) ? 1 : 0
+  );
+  console.table({ randomErr, syndrome, indexes, correctVec });
+};
+
+const findHIndexes = (
+  vec: number[],
+  H: number[][],
+  multiplicity: number
+): number[] => {
+  let res = [-1, -1];
+  H.forEach((row, index) => {
+    if (areEqual(row, vec)) {
+      res = [index];
+    }
+    if (multiplicity !== 1) {
+      H.slice(index + 1).forEach((rowAfter, indexAfter) => {
+        if (areEqual(bitXor(row, rowAfter), vec)) {
+          res = [index, indexAfter + index + 1];
+        }
+      });
+    }
+  });
+  return res;
+};
+
+const areEqual = (a: any, b: any) => JSON.stringify(a) === JSON.stringify(b);
+
+func();
+
+// solve(10, 3, 5, 2);
